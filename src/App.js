06c8f6083e38f7c09d93d1330bb21fbf0bb4e163
Sergey Gapochka <gapochka.sergey@gmail.com>
2021-02-17 23:09:57 +0300

    feat(app.js): add select for app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-import './App.css';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const App = () => <p>Hello!</p>;
+import { changeValue } from 'store/actions';
+import { getValue } from 'selectors';
+import Select from 'components/Select';
+
+import './App.css';
+import items from './items.json';
+
+const filterItems = inputValue => items.filter(i => i.label.toLowerCase().includes(inputValue.toLowerCase()));
+
+const loadOptions = (inputValue, callback) => {
+  setTimeout(() => {
+    callback(filterItems(inputValue));
+  }, 1000);
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { label } = useSelector(getValue);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = value => setInputValue(value);
+  const handleBlur = () => setInputValue('');
+  const handleChange = value => dispatch(changeValue(value));
+
+  return (
+    <>
+      <p>inputValue: &quot;{inputValue}&quot;</p>
+      <Select
+        value={inputValue}
+        placeholder={label}
+        loadOptions={loadOptions}
+        onBlur={handleBlur}
+        onInputChange={handleInputChange}
+        onChange={handleChange}
+      />
+    </>
+  );
+};
 
 export default App;
