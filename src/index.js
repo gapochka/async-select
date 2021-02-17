90cd98512ea8759030a7447a85b621ffe6c4c321
Sergey Gapochka <gapochka.sergey@gmail.com>
2021-02-17 23:06:31 +0300

    feat(store): connect to store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css';
+
+import store from 'store/store';
 
 import App from './App';
 import './index.css';
-import 'normalize.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
