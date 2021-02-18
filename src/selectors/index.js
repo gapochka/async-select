fd6efbad492e6f2fb8dc1506533bea5d2ab7e9cf
Sergey Gapochka <gapochka.sergey@gmail.com>
2021-02-18 03:44:55 +0300

    refactor(selectors): remove eslint-disable

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,6 +1,5 @@
 import { createSelector } from 'reselect';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getValue = createSelector(
   state => state,
   ({ value, label }) => ({ value, label })
